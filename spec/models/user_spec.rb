0f5af0342112dd637011085b2a337c44db58c92e
kyohhh <khnm0322@gmail.com>
2021-01-13 21:47:50 +0900

    メンター修正_1_不要なファイル削除.テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,27 +10,6 @@ RSpec.describe User, type: :model do
       it 'nicknameとemailとpasswordとpassword_confirmationとfamily_nameとfirst_nameとfamily_name_kanaとfirst_name_kanaとbirthdayが存在すれば登録できる' do
         expect(@user).to be_valid
       end
-      it 'passwordとpassword_confirmationが6文字以上の半角英数字混合だと登録できる' do
-        @user.password = 'aaa111'
-        @user.password_confirmation = 'aaa111'
-        expect(@user).to be_valid
-      end
-      it 'family_nameは全角（漢字・ひらがな・カタカナ）だと登録できる' do
-        @user.family_name = '亜あア'
-        expect(@user).to be_valid
-      end
-      it 'first_nameは全角（漢字・ひらがな・カタカナ）だと登録できる' do
-        @user.first_name = '亜あア'
-        expect(@user).to be_valid
-      end
-      it 'family_name_kanaは全角(カタカナ)だと登録できる' do
-        @user.family_name_kana = 'ア'
-        expect(@user).to be_valid
-      end
-      it 'first_name_kanaは全角(カタカナ)だと登録できる' do
-        @user.first_name_kana = 'ア'
-        expect(@user).to be_valid
-      end
     end
 
     context '新規登録できないとき' do
@@ -91,6 +70,11 @@ RSpec.describe User, type: :model do
         @user.password = 'aaa11'
         @user.valid?
         expect(@user.errors.full_messages).to include 'Password is too short (minimum is 6 characters)'
+      end
+      it 'passwordは全角英数字だと登録できない' do
+        @user.password = 'ａａａ１１１'
+        @user.valid?
+        expect(@user.errors.full_messages).to include "Password is invalid"
       end
       it 'passwordは半角数字のみだと登録できない' do
         @user.password = '111111'
