b8a22792dd87129a40b98a2c25f0829caaf43cbc
kyohhh <khnm0322@gmail.com>
2021-01-13 21:04:04 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,13 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :encrypted_password,:password,:password_confirmation,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}  
-  validates :family_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :family_name_kana, presence: true,format: { with: /\A[ァ-ヶー－]+\z/}
-  validates :first_name_kana, presence: true,format: { with: /\A[ァ-ヶー－]+\z/}
+  validates :encrypted_password, :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
-  
+
   has_many :items
   has_many :purchases
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname                        { Faker::Name.name }
     email                           { Faker::Internet.email }
-    password                        { "aaa111" }
+    password                        { 'aaa111' }
     password_confirmation           { password }
-    family_name                     { "ぜんかく" }
-    first_name                      { "ぜんかく" }
-    family_name_kana                { "ゼンカクカナ" }
-    first_name_kana                 { "ゼンカクカナ" }
-    birthday                        { "2021-01-01" }
+    family_name                     { 'ぜんかく' }
+    first_name                      { 'ぜんかく' }
+    family_name_kana                { 'ゼンカクカナ' }
+    first_name_kana                 { 'ゼンカクカナ' }
+    birthday                        { '2021-01-01' }
   end
 end
-
