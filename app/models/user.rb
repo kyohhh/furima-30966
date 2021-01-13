76f6624c41da3a8f3812a78ee52e1f29d8ed3ef5
kyohhh <khnm0322@gmail.com>
2021-01-13 22:19:57 +0900

    メンター修正_2_with_options記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :encrypted_password, :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :family_name
+      validates :first_name
+    end
+    with_options format: {with: /\A[ァ-ヶー－]+\z/ } do
+      validates :family_name_kana
+      validates :first_name_kana
+    end
+    validates :birthday
+  end
 
-  validates :nickname, presence: true
-  validates :email, presence: true
-  validates :encrypted_password, :password, :password_confirmation, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ }
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
-
-  has_many :items
-  has_many :purchases
+    has_many :items
+    has_many :purchases
 end
