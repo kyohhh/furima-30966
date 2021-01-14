f1c645f55ee86502864e8a6554a6f3471ffc9529
kyohhh <khnm0322@gmail.com>
2021-01-14 16:28:00 +0900

    モデル.テーブル作成_Active Storage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  #def new
+    #@item = Item.new
+  #end
+
+  #def create
+    #Item.create(item_params)
+  #end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :state_id, :shipping_charges_id, :prefecture_id, :days_to_ship_id, :price, :user).merge(user_id: current_user.id)
+  end
 end
