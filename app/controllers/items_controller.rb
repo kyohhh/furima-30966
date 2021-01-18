d55be5ed6b921b5678e045500ccc1d49ba5aa24a
kyohhh <khnm0322@gmail.com>
2021-01-18 23:12:30 +0900

    Revert "商品出品ページを表示"
    This reverts commit 86f130da2823bcc1e9a8dbb55aeaa993b5f8703c.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,17 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
-
   def index
   end
 
-  def new
-    @item = Item.new
-  end
+  #def new
+    #@item = Item.new
+  #end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.valid?
-      @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+  #def create
+    #Item.create(item_params)
+  #end
 
   private
-  
   def item_params
-    params.require(:item).permit(:image, :item_name, :description, :category_id, :state_id, :shipping_charges_id, :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :description, :category_id, :state_id, :shipping_charges_id, :prefecture_id, :days_to_ship_id, :price, :user).merge(user_id: current_user.id)
   end
 end
