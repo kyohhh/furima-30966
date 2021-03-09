1398bb127c6048a260e5654e08c529aa1445ab5b
kyohhh <khnm0322@gmail.com>
2021-03-10 01:40:44 +0900

    jQueryを導入_slickの導入

---
+++ app/javascript/slick.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.slider').slick({
+    prevArrow: '<i class="fas fa-arrow-alt-circle-left"></i>',
+    nextArrow: '<i class="fas fa-arrow-alt-circle-right"></i>',
+    autoplay: true,
+    pauseOnFocus: false,
+    autoplaySpeed: 3000,
+    slidesToShow: 1,
+    centerMode: true,
+    centerPadding: '100px',
+    focusOnSelect: true,
+    dots: true,
+    variableWidth: true
+  })
+});
