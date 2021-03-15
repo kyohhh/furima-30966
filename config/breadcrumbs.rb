32f6e601e66844142d26a3e424eef3423af0c015
kyohhh <khnm0322@gmail.com>
2021-03-15 21:10:14 +0900

    パンくずリスト作成_2

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,17 +2,17 @@ crumb :root do
   link "トップ", root_path
 end
 
- crumb :items do |item|
+ crumb :show_item do |item|
    link "商品詳細", item_path(item)
    parent :root
  end
 
 crumb :orders do |item|
   link "購入内容の確認", item_orders_path
-  parent :items, item
+  parent :show_item, item
 end
 
-#crumb :items do |item|
-  #link "商品の編集", edit_item_path
-  #parent :items, item
-#end
+crumb :edit_item do |item|
+  link "商品編集", edit_item_path(item)
+  parent :show_item, item
+end
