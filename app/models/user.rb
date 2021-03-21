b35a2097777121fca8fc2c800da7f7ea5b5b4fc2
kyohhh <khnm0322@gmail.com>
2021-03-21 20:57:27 +0900

    Rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,16 @@ class User < ApplicationRecord
     validates :nickname, presence: { message: 'を入力してください' }
     validates :email
     validates :password, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/,
-                                   message: 'は半角英数字で入力してください'  }
-    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/,message: 'は全角で入力してください' } do
+                                   message: 'は半角英数字で入力してください' }
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'は全角で入力してください' } do
       validates :family_name
       validates :first_name
     end
-    with_options format: { with: /\A[ァ-ヶー－]+\z/,message: 'は全角カナで入力してください' } do
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カナで入力してください' } do
       validates :family_name_kana
       validates :first_name_kana
     end
-    validates :birthday,presence: { message: 'を選択してください' }
+    validates :birthday, presence: { message: 'を選択してください' }
   end
 
   has_many :items
--- spec/models/user_order_spec.rb
+++ spec/models/user_order_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe UserOrder, type: :model do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:item)
     sleep(0.1)
-    @user_order = FactoryBot.build(:user_order, user_id: @user.id , item_id: @item.id)
+    @user_order = FactoryBot.build(:user_order, user_id: @user.id, item_id: @item.id)
   end
   describe '商品購入機能' do
     context '商品購入できる場合' do
