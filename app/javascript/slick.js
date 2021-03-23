07e727f661217dd3daca74a571927d5a90d825ba
kyohhh <khnm0322@gmail.com>
2021-03-23 15:59:04 +0900

    slick修正_2

--- app/javascript/slick.js
+++ app/javascript/slick.js
@@ -1,7 +1,7 @@
 $(function() {
   $('.slider').slick({
-    prevArrow: '<i class="fas fa-arrow-alt-circle-left"></i>',
-    nextArrow: '<i class="fas fa-arrow-alt-circle-right"></i>',
+    //prevArrow: '<i class="fas fa-arrow-alt-circle-left"></i>',
+    //nextArrow: '<i class="fas fa-arrow-alt-circle-right"></i>',
     autoplay: true,
     pauseOnFocus: false,
     autoplaySpeed: 3000,
